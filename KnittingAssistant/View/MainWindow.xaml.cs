86b60be15c021f2889baf03d3c57bdf1efda9ae4
gunpowder115 <87995472+gunpowder115@users.noreply.github.com>
2022-11-12 13:46:59 +0300

    All areas from main window moved to different user controls

--- KnittingAssistant/View/MainWindow.xaml.cs
+++ KnittingAssistant/View/MainWindow.xaml.cs
@@ -41,52 +41,5 @@ namespace KnittingAssistant.View
         {
             DataContext = new MainViewModel();
         }
-
-        private void click_LoadImageOnForm(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog fileDialogPicture = new OpenFileDialog();
-            fileDialogPicture.Filter = "Изображения|*.bmp;*.jpg;*.gif;*.png;*.tif";
-            fileDialogPicture.FilterIndex = 1;
-
-            if (fileDialogPicture.ShowDialog() == true)
-            {
-                string imageFilename = fileDialogPicture.FileName;
-                loadImageOnForm(imageFilename);
-            }
-        }
-
-        private void drop_LoadImageOnForm(object sender, DragEventArgs e)
-        {
-            string imageFilename = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
-            if (imageFilename.Contains(".bmp") || imageFilename.Contains(".jpg") ||
-                imageFilename.Contains(".gif") || imageFilename.Contains(".png") ||
-                imageFilename.Contains(".tif"))
-            {
-                loadImageOnForm(imageFilename);
-            }
-
-        }
-
-        private void loadImageOnForm(string imageFilename)
-        {
-            mainImage.Source = new BitmapImage(new Uri(imageFilename));
-            imageLoaded = true;
-            (DataContext as MainViewModel).SetSettingsIsEnabled(imageLoaded);
-            getMainlImageSize();
-            setDisplayImageSize();
-        }
-
-        private void getMainlImageSize()
-        {
-            mainImageWidth = mainImage.Source.Width;
-            mainImageHeight = mainImage.Source.Height;
-            mainImageRatio = mainImageWidth / mainImageHeight;
-            (DataContext as MainViewModel).MainImageRatio = mainImageRatio;
-        }
-
-        private void setDisplayImageSize()
-        {
-            (DataContext as MainViewModel).SetDisplayImageSize(100.0, 100.0 / mainImageRatio);
-        }
     }
 }
---
+++ KnittingAssistant/View/userControls/ImageArea.xaml.cs
@@ -0,0 +1,54 @@
+﻿using KnittingAssistant.ViewModel;
+using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace KnittingAssistant.View.userControls
+{
+    /// <summary>
+    /// Логика взаимодействия для ImageArea.xaml
+    /// </summary>
+    public partial class ImageArea : UserControl
+    {
+        private bool imageLoaded;
+
+        public ImageArea()
+        {
+            InitializeComponent();
+        }
+
+        private void click_LoadImageOnForm(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialogPicture = new OpenFileDialog();
+            fileDialogPicture.Filter = "Изображения|*.bmp;*.jpg;*.gif;*.png;*.tif";
+            fileDialogPicture.FilterIndex = 1;
+
+            if (fileDialogPicture.ShowDialog() == true)
+            {
+                string imageFilename = fileDialogPicture.FileName;
+                loadImageOnForm(imageFilename);
+            }
+        }
+
+        private void drop_LoadImageOnForm(object sender, DragEventArgs e)
+        {
+            string imageFilename = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+            if (imageFilename.Contains(".bmp") || imageFilename.Contains(".jpg") ||
+                imageFilename.Contains(".gif") || imageFilename.Contains(".png") ||
+                imageFilename.Contains(".tif"))
+            {
+                loadImageOnForm(imageFilename);
+            }
+
+        }
+
+        private void loadImageOnForm(string imageFilename)
+        {
+            mainImage.Source = new BitmapImage(new Uri(imageFilename));
+            imageLoaded = true;
+            (DataContext as MainViewModel).SetSettingsIsEnabled(imageLoaded);
+        }
+    }
+}
