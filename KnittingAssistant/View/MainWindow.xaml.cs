6ee03a1a442f35db11aabc2d06c4c698bdf03ba6
gunpowder115 <87995472+gunpowder115@users.noreply.github.com>
2022-12-06 22:25:19 +0300

    DataContext creating moved to XAML

--- KnittingAssistant/View/MainWindow.xaml.cs
+++ KnittingAssistant/View/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System.Windows;
-using KnittingAssistant.ViewModel;
-using KnittingAssistant.View.userControls;
 
 namespace KnittingAssistant.View
 {
@@ -12,13 +10,6 @@ namespace KnittingAssistant.View
         public MainWindow()
         {
             InitializeComponent();
-
-            this.Loaded += MainWindow_Loaded;
-        }
-
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            DataContext = new MainViewModel();
         }
     }
 }
