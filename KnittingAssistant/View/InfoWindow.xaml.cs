860fded06d7a391fab2f47a8359edb45bc253af6
gunpowder115 <87995472+gunpowder115@users.noreply.github.com>
2023-05-03 19:04:30 +0300

    Added info window

--- KnittingAssistant/View/InfoWindow.xaml.cs
+++ KnittingAssistant/View/InfoWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using KnittingAssistant.ViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace KnittingAssistant.View
 {
@@ -22,6 +11,13 @@ namespace KnittingAssistant.View
         public InfoWindow()
         {
             InitializeComponent();
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = new InfoViewModel();
         }
     }
 }
---
+++ KnittingAssistant/ViewModel/InfoViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace KnittingAssistant.ViewModel
+{
+    public class InfoViewModel : ViewModelBase
+    {
+        private const string VK_LINK = "https://vk.com/vlad_poroh";
+        private const string GITHUB_LINK = "https://github.com/gunpowder115/KnittingAssistant";
+
+        #region Dependency Properties
+
+        private FlowDocument infoText;
+        public FlowDocument InfoText
+        {
+            get { return infoText; }
+            set
+            {
+                infoText = value;
+                OnPropertyChanged("InfoText");
+            }
+        }
+
+        #endregion
+
+        #region Relay Commands
+
+        private RelayCommand vkLinkClickCommand;
+        public RelayCommand VkLinkClickCommand
+        {
+            get
+            {
+                return vkLinkClickCommand ??
+                    (vkLinkClickCommand = new RelayCommand(obj =>
+                    {
+                        try
+                        {
+                            Process.Start(new ProcessStartInfo(VK_LINK) { UseShellExecute = true } );
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }));
+            }
+        }
+
+        private RelayCommand githubLinkClickCommand;
+        public RelayCommand GithubLinkClickCommand
+        {
+            get
+            {
+                return githubLinkClickCommand ??
+                    (githubLinkClickCommand = new RelayCommand(obj =>
+                    {
+                        try
+                        {
+                            Process.Start(new ProcessStartInfo(GITHUB_LINK) { UseShellExecute = true } );
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }));
+            }
+        }
+
+        #endregion
+    }
+}
